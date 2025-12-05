9316280662b8c38d799c7a397274d2491f4aa457
Dev-web-god <anarq5260q@gmail.com>
2025-12-05 19:22:41 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+document.getElementById("blow").addEventListener("click", function(e) {
+    e.preventDefault();
+
+    document.querySelectorAll(".fire").forEach(function(el) {
+        el.style.display = "none";
+    });
+    confetti({
+        particleCount: 800,
+        spread: 60,
+        origin: { x: 0, y: 0.6 }
+    });
+    confetti({
+        particleCount: 800,
+        spread: 60,
+        origin: { x: 1, y: 0.6 }
+    });
+    confetti({
+        particleCount: 800,
+        spread: 60,
+        startVelocity: 40,
+        gravity: 2,
+        origin: { x: 0.5, y: 0 }
+    });
+});
