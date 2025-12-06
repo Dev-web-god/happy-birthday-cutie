fe783bc7dfd2e5c5a5f06e200732aa040ddb0278
Dev-web-god <anarq5260q@gmail.com>
2025-12-06 22:48:33 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-const age = document.getElementById("age");
-document.getElementById("blow").addEventListener("click", function(e) {
+document.getElementById("blow").addEventListener("click", function (e) {
     e.preventDefault();
 
-    document.querySelectorAll(".fire").forEach(function(el) {
+    document.querySelectorAll(".fire").forEach(function (el) {
         el.style.display = "none";
     });
-    age.textContent = "18";
     confetti({
         particleCount: 800,
         spread: 60,
@@ -23,4 +21,22 @@ document.getElementById("blow").addEventListener("click", function(e) {
         gravity: 2,
         origin: { x: 0.5, y: 0 }
     });
+});
+function openCard() {
+    let frame = document.getElementById("mainFrame");
+
+    frame.style.display = "block";
+
+    frame.src = "card.html";
+
+    document.querySelector("svg").style.display = "none";
+    document.querySelector(".candle").style.display = "none";
+    document.querySelector(".text").style.display = "none";
+}
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    let audio = document.getElementById("bgmusic");
+    audio.muted = false;
+    audio.play().catch(()=>{}); 
+  }, 10);
 });
